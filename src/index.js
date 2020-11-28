0faf874093a95831b14a26b2c46845bc0cc34302
Or Shalmayev <46129649+OrShalmayev@users.noreply.github.com>
2020-11-28 13:14:26 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,16 +3,12 @@ function defaultEqualityCheck(a, b) {
 }
 
 function areArgumentsShallowlyEqual(equalityCheck, prev, next) {
-  if (prev === null || next === null || prev.length !== next.length) {
-    return false
-  }
+  if (prev === null || next === null || prev.length !== next.length) return false
 
   // Do this in a for loop (and not a `forEach` or an `every`) so we can determine equality as fast as possible.
   const length = prev.length
   for (let i = 0; i < length; i++) {
-    if (!equalityCheck(prev[i], next[i])) {
-      return false
-    }
+    if (!equalityCheck(prev[i], next[i])) return false
   }
 
   return true
